96daf983569441f6e933d94683162c9f6cb31121
Rasmus Viitanen <rasmus.viitanen@helsinki.fi>
2024-11-15 15:36:00 +0200

    updated handle posts

--- src/modules/routes.py
+++ src/modules/routes.py
@@ -10,11 +10,19 @@ def index():
 @app.route("/create_reference/article", methods=["GET", "POST"])
 def add_ref():
     if request.method == "GET":
-        return render_template("create_reference_article.html")
+        return render_template("create_reference_article.html", error=False)
     if request.method == "POST":
-        author = request.form.get("author")
-        title = request.form.get("title")
-        journal = request.form.get("journal")
-        year = request.form.get("year")
+        try:
+            author = request.form.get("author")
+            title = request.form.get("title")
+            journal = request.form.get("journal")
+            year = int(request.form.get("year"))
+
+        except ValueError:
+            return render_template("create_reference_article.html", error=True, error_message="Virheelliset tiedot")
+
         if database.add_article(author, title, journal, year):
             return redirect(url_for("index"))
+        else:
+            return render_template("create_reference_article.html", error=True, error_message="Virheelliset tiedot")
+
