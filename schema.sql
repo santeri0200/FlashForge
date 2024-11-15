3aee20eb6164c08292a60a68f75c681a7b034773
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-11-15 14:20:12 +0200

    Added articles to the database

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS Articles (
+    id SERIAL PRIMARY KEY,
+    -- Required fields
+    author  TEXT NOT NULL,
+    title   TEXT NOT NULL,
+    journal TEXT NOT NULL,
+    year    INT  NOT NULL,
+    -- Optional fields
+    volume  INT,
+    number  INT,
+    pages   INT,
+    month   INT,
+    note    TEXT,
+);
