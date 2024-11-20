4fe984b6f81e7cfe9a57a3eef00fb5681b568cd5
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-11-20 12:09:47 +0200

    Removed unnecessary dotenv import

--- src/config.py
+++ src/config.py
@@ -1,10 +1,6 @@
 from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()
 
 test_env = getenv("TEST_ENV") == "true"
 print(f"Test environment: {test_env}")
