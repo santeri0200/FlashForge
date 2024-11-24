b7e63c2a8a1c9e2f7c80cb51d949b4ce14dc2be6
Ville Lampiselkä <148494501+pondbackjunior@users.noreply.github.com>
2024-11-25 01:08:00 +0200

    add functionality to edit and delete article refs

--- src/modules/database.py
+++ src/modules/database.py
@@ -10,12 +10,36 @@ def add_article(author, title, journal, year):
     db.session.commit()
     return True
 
+def edit_article(id, author, title, journal, year):
+    try:
+        sql = text("UPDATE articles SET author=:author, title=:title, journal=:journal, year=:year WHERE id=:id")
+        db.session.execute(sql, {"author": author, "title": title, "journal": journal, "year": year, "id":id})
+    except:
+        return False
+    db.session.commit()
+    return True
+
+def delete_article(id):
+    try:
+        sql = text("DELETE FROM articles WHERE id=:id")
+        db.session.execute(sql, {"id":id})
+    except:
+        return False
+    db.session.commit()
+    return True
+
 def get_all_articles():
-    sql = text("SELECT author, title, journal, year FROM articles ORDER BY id DESC")
+    sql = text("SELECT id, author, title, journal, year FROM articles ORDER BY id DESC")
     res = db.session.execute(sql)
 
     articles = res.fetchall()    
     return articles
+
+def article_from_id(id):
+    sql = text(f"SELECT id, author, title, journal, year FROM articles WHERE id={id} LIMIT 1")
+    res = db.session.execute(sql)
+    article = res.fetchone()
+    return article
 
 def reset_db():
     print(f"Clearing contents from table articles")
