72a55474a3a4c418e458ef1beb02f2c745903e2b
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-12-09 10:37:17 +0200

    Added "manuals" and reformated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Articles (
-    id SERIAL PRIMARY KEY,
+    id      SERIAL PRIMARY KEY,
     -- Required fields
     author  TEXT NOT NULL,
     title   TEXT NOT NULL,
@@ -16,34 +16,48 @@ CREATE TABLE IF NOT EXISTS Articles (
 );
 
 CREATE TABLE IF NOT EXISTS Books (
-    id SERIAL PRIMARY KEY,
+    id        SERIAL PRIMARY KEY,
     -- Required fields
-    author      TEXT NOT NULL,
-    year        INT  NOT NULL,
-    title       TEXT NOT NULL,
-    publisher   TEXT NOT NULL,
-    address     TEXT NOT NULL,
+    author    TEXT NOT NULL,
+    year      INT  NOT NULL,
+    title     TEXT NOT NULL,
+    publisher TEXT NOT NULL,
+    address   TEXT NOT NULL,
 
     UNIQUE (author, title)
 );
 
 CREATE TABLE IF NOT EXISTS Inproceedings (
-    id SERIAL PRIMARY KEY,
+    id           SERIAL PRIMARY KEY,
     -- Required fields
-    author      TEXT NOT NULL,
-    title       TEXT NOT NULL,
-    booktitle   TEXT NOT NULL,
-    year        INT  NOT NULL,
+    author       TEXT NOT NULL,
+    title        TEXT NOT NULL,
+    booktitle    TEXT NOT NULL,
+    year         INT  NOT NULL,
     -- Optional fields
-    editor          TEXT,
-    volume          INT,
-    number          INT,
-    series          TEXT,
-    pages           TEXT,
-    address         TEXT,
-    month           TEXT,
-    organization    TEXT,
-    publisher       TEXT,
+    editor       TEXT,
+    volume       INT,
+    number       INT,
+    series       TEXT,
+    pages        TEXT,
+    address      TEXT,
+    month        TEXT,
+    organization TEXT,
+    publisher    TEXT,
 
-    UNIQUE (author, title)
+    UNIQUE (author, booktitle)
+);
+
+CREATE TABLE IF NOT EXISTS Manuals (
+    id           SERIAL PRIMARY KEY,
+    -- Required fields
+    title        TEXT NOT NULL,
+    year         INT  NOT NULL,
+    -- Optional fields
+    author       TEXT,
+    organization TEXT,
+    address      TEXT,
+    edition      TEXT,
+    month        TEXT,
+    note         TEXT,
 );
